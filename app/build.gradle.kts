54558ce9fe3647968efe0fee2d65a48feedd2681
AMaslyakov <a.v.masliakov@urfu.me>
2025-04-16 14:40:47 +0500

    Добавлена библиотека Android SVG
     Add AndroidSVG lib and simple test app.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    implementation(libs.sf.wkt)
+    implementation(libs.androidsvg.aar)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- app/src/main/java/com/example/parkingapp/MainActivity.java
+++ app/src/main/java/com/example/parkingapp/MainActivity.java
@@ -1,5 +1,19 @@
 package com.example.parkingapp;
 
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.PictureDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.caverock.androidsvg.SVG;
+import com.caverock.androidsvg.SVGParseException;
+
+import java.io.IOException;
+
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,19 +22,35 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        EdgeToEdge.enable(this);
-        setContentView(new ParkingAppView(this));
 
+        EdgeToEdge.enable(this);
 
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-//            return insets;
-//        });
+        setContentView(R.layout.activity_main);
+
+//        LinearLayout l = findViewById(R.id.main);
+
+//        LinearLayout layout = new LinearLayout(this);
+
+//        SVGImageView svgImageView = new SVGImageView(this);
+//        svgImageView.setImageAsset("map1.svg");
+//        layout.addView(svgImageView,
+//                       new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
+//        setContentView(layout);
+
+        ImageView imgV = findViewById(R.id.imageView);
+        imgV.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+        try{
+            SVG svg = SVG.getFromAsset(this.getAssets(), "map1.svg");
+            Drawable drawable = new PictureDrawable(svg.renderToPicture());
+            imgV.setImageDrawable(drawable);
+        }catch (SVGParseException | IOException e){
+            System.out.println(e);
+        }
     }
 }
